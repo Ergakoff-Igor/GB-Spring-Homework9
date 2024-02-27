e3b97c138cb98042edddfed98ebdcfb6668cfcfb
Ierga <i.ergakoff@gmail.com>
2024-02-27 23:26:46 +0300

    GB. Spring.Homework 9. Final

--- apigateway/src/main/java/com/example/task_1_1/Task11Application.java
+++ apigateway/src/main/java/com/example/task_1_1/Task11Application.java
@@ -16,8 +16,8 @@ public class Task11Application {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("Microservice1",r->r.path("/serviceA/**")
-                        .uri("http://localhost:8081/"))
-                .route("Microservice2",r->r.path("/serviceB/**")
-                        .uri("http://localhost:8082/")).build();}
+                .route("Microservice1",r->r.path("/acts/**")
+                        .uri("http://localhost:8080/"))
+                .route("Microservice2",r->r.path("/users/**")
+                        .uri("http://localhost:8081/")).build();}
 }
